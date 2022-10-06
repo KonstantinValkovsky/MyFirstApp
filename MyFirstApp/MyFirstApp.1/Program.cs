955a675d6e07e267cf9ada6479be0a8b3ae22d1a
Костя Вальковский <kostya.valkovski.work@gmail.com>
2022-10-06 23:00:41 +0300

    Модуль 3. Основы C# и CLR. Основные понятия и типы данных  3.7. Преобразования типов

--- MyFirstApp/MyFirstApp.1/Program.cs
+++ MyFirstApp/MyFirstApp.1/Program.cs
@@ -6,48 +6,24 @@ namespace MyFirstApp._1
     {
         static void Main(string[] args)
         {
-            const string MyName = "Kostya";
-            Console.WriteLine(MyName);
+            Console.Write("Как вас зовут? ");
+            var name = Console.ReadLine();
 
-            Console.WriteLine("\tПривет Мир!");
-            Console.WriteLine("\tМне 23 года.");
-            Console.WriteLine("\tMy name is Kostya\n");
+            Console.Write("Сколько вам лет? ");
+            var age = Console.ReadLine();
 
-            Console.WriteLine("Привет,\n Мир!");
+            Console.WriteLine($"Вас зовут {name} и вам {age}!");
 
-            Console.WriteLine('\x23');
+            Console.Write("Когда вы роделись? ");
+            var birthdate = Console.ReadLine();
 
-            byte MyAge = 23;
-            bool pet = true;
-            double sizeshoes = 45;
-
-            Console.WriteLine("\nMy name is " + MyName);
-            Console.WriteLine("My Age " + MyAge);
-            Console.WriteLine("Do you have a pet?\t " + pet);
-            Console.WriteLine("My shoe size is " + sizeshoes);
-            Console.WriteLine();
-
-            DaysOfWeek MyFavoriteDay;
-            MyFavoriteDay = DaysOfWeek.Friday;
-            Console.WriteLine(MyFavoriteDay);
+            Console.WriteLine($"Вы роделись {birthdate}!");
 
 
             Console.ReadKey();
         }
-        enum DaysOfWeek : byte
-            {
-            Tuesday,
-            Monday,
-            Wednesday,
-            Friday
-        }
 
-        enum Semaphore
-        {
-            Red = 100,
-            Yellow = 200,
-            Green = 300
-        }
+
 
     }
 }
